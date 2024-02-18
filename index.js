cedf98cdddd00007baa6c9866b7cbfc0e9c86876
Tyler M. Neher <tyler@neherdata.com>
2024-02-18 13:35:43 -0500

    redo
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const apiKey =
-  "v1.public.eyJqdGkiOiJjZGEyMDBmNS1kN2RmLTQyNjktOWEyOC1iMDIxZDI3N2FhZGQifVy4F-kq1lfFOXxS_0V4-tT5rOa57ietpc-P8toXSmYo3c0oPwyTVQ00WcUbaJKDwq8jlCud0zmq50cRBfFGOWfUlVNy1I--FJweVK4j-WdSG7S-1Vf8E-TZbCk_ooceUZOhj9LQD4MD5HlZbaAAP6syK4Hz2wzsyjhGgEzk5y7bAGLdOmaNdJ2eFtGwK2XGpQfA8sKsfnwgevGWCAL77BdsHzaJMNrbKc9WXkIgt2VxvvoKk9qVzlrubYB5tMu_u09ZRuM6RlTxCV8xOTmTqlTKC20QdZo7DeRtx-lKY_XgrBYKPnwn2VXfpsyb5UKsP-AeFg_OzRe7GuXo36Sp55E.ZWU0ZWIzMTktMWRhNi00Mzg0LTllMzYtNzlmMDU3MjRmYTkx";
+const apiKey = "v1.public.eyJqdGkiOiI1YWE4OWU5Yy1kZTAzLTQ2MjQtYmI5OS05NWNiMDc3NmViMjMifQiQa2xfpxCsmRGGyaNZxHB_-WS6fYvG2je5SFdPJdwo6f2C5uQtbRfR6L-jjc8nOqGnb4f3T5Sn3BYDxWXJT3VENDiSRzg9bDX8y3oUzU4nPMITEPaKxwSkA2Vfiuhl4Jfl8dVvh2Tn-WmOoAePy9TJHSt5KJeTDm2KRKaaA8YSBSzuWzHXXgQlUlI2a1NQDkBVPc_8On4y-zQnVjPTWo2xXDl1BC-oiWCuZY-K-zm3SSgt28pHobZyzBH2OG88h9pVMWJ01G0cLU7ybPsqO585m8SLcXgDzAbEMggTR1qSCGDjR2USdXLE9SdLLaEw58Vutu9gsaMkufm8AHqu3Co.ZWU0ZWIzMTktMWRhNi00Mzg0LTllMzYtNzlmMDU3MjRmYTkx";
+const mapName = "aws-maps.neherdata.com";
 const region = "us-east-1";
-const mapName = "ndsMap";
-const styleUrl = `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor?key=${apiKey}`;
+
 const map = new maplibregl.Map({
   container: "map",
-  style: styleUrl,
+  style: `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor?key=${apiKey}`,
   center: [-74.0697479, 40.3083382],
   zoom: 11,
 });
